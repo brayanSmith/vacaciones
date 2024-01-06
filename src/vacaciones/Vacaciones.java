37ceb899091a1a768e649f3a38a8236d5c4ce3bf
User <brayan.suarezsegura308@gmail.com>
2024-01-06 00:30:27 -0500

    Carga InicialV1

--- src/vacaciones/Vacaciones.java
+++ src/vacaciones/Vacaciones.java
@@ -66,7 +66,8 @@ public class Vacaciones {
                 System.out.println("Revise que no halla digitado mal algun dato");
             }
 
-            System.out.println("[1] reiniciar / [2] salir");
+            System.out.println("[1] reiniciar - [2] salir");
+            
             reiniciar = in.nextInt();
 
         } while (reiniciar == 1);
